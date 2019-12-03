9863c6c556f5709d52a6e4d300f3448db617b606
huli1984 <gabri.kitzune@gmail.com>
2019-12-03 12:03:05 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='adspy',
+    version="0.0.1",
+    description='engine for parsing ADwords in ADsPy django',
+    author='Gabriele Santucci',
+    maintainer='Gabriele Santucci',
+    maintainer_email='gabrielesantucci1983@gmail.com',
+    url='https://github.com/huli1984/adspy_public',
+    license='',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    install_requires=['requests', 'numpy', 'bs4', 'selenium', 'pandas'],
+    tests_require=['requests', 'numpy', 'bs4', 'selenium', 'pandas'],
+)
